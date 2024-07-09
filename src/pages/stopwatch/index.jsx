6335ab7ce8e9ad6c870d1f73b6b8c225aa7da4f1
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-07-09 18:19:28 +0530

    Added basic layout for stopwatch component

--- src/pages/stopwatch/index.jsx
+++ src/pages/stopwatch/index.jsx
@@ -1,5 +1,24 @@
+import { Button, Col, Row } from "antd";
+
 const Stopwatch = () => {
-  return <div>Stopwatch</div>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>1s</h1>
+      <Row justify="center" gutter={[16, 16]}>
+        <Col>
+          <Button type="primary">Start</Button>
+        </Col>
+        <Col>
+          <Button type="primary" danger>
+            Stop
+          </Button>
+        </Col>
+        <Col>
+          <Button>Reset</Button>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Stopwatch;
